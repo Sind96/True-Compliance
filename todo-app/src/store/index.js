6ef04010e6fa8212267009df6d0a2370cd8b85cb
Sind96 <sindhujany@gmail.com>
2024-09-15 14:50:40 +0100

    Feat: Updare the Vuex Store

---
+++ todo-app/src/store/index.js
@@ -0,0 +1,31 @@
+import { createStore } from "vuex";
+
+export const store = createStore({
+  state: {
+    tasks: [],
+  },
+  mutations: {
+    addTask(state, task) {
+      state.tasks.push(task);
+    },
+    toggleTaskCompletion(state, taskId) {
+      const task = state.tasks.find((task) => task.id === taskId);
+      task.completed = !task.completed;
+    },
+    deleteTask(state, taskId) {
+      state.tasks = state.tasks.filter((task) => !task.completed);
+    },
+    setTasks(state, tasks) {
+      state.tasks = tasks;
+    },
+  },
+  actions: {
+    saveTasks({ state }) {
+      localStorage.setItem("tasks", JSON.stringify(state.tasks));
+    },
+    loadTasks({ commit }) {
+      const tasks = JSON.parse(localStorage.getItem("tasks"));
+      commit("setTasks", tasks);
+    },
+  },
+});
