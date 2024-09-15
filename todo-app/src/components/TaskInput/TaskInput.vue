753c0a236d47aa8586bcf8215a34e0f76cfed5ed
Sind96 <sindhujany@gmail.com>
2024-09-15 15:34:03 +0100

    Feat: TaskInput component done

--- todo-app/src/components/TaskInput/TaskInput.vue
+++ todo-app/src/components/TaskInput/TaskInput.vue
@@ -7,3 +7,24 @@
     class="input"
   />
 </template>
+
+<script>
+export default {
+  data() {
+    return { newTask: "" };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim()) {
+        this.$store.commit("addTask", {
+          id: Date.now(),
+          text: this.newTask,
+          completed: false,
+        });
+        this.newTask = "";
+        this.$store.dispatch("saveTasks");
+      }
+    },
+  },
+};
+</script>
