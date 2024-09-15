cba8d7911c49b826802bb505b391b596f70a13d2
Sind96 <sindhujany@gmail.com>
2024-09-15 15:09:34 +0100

    Feat: Add TaskInput onto App.vue

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,1 +1,9 @@
+<script setup>
+import TaskInput from "./components/TaskInput/TaskInput.vue";
+</script>
 
+<template>
+  <div>
+    <TaskInput />
+  </div>
+</template>
--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
+import { store } from "./store";
 import App from "./App.vue";
 import "./assets/tailwind.css";
-import { store } from "./store";
 
 const app = createApp(App);
 app.use(store);
